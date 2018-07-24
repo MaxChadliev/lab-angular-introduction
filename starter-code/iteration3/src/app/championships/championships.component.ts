ecaa2fdcc826b271c25adbf4aacf64baf1b8d5d1
Maxim Chadliev <chad0441@gmail.com>
2018-07-24 10:12:13 -0400

    iteration 3 complete

--- starter-code/iteration3/src/app/championships/championships.component.ts
+++ starter-code/iteration3/src/app/championships/championships.component.ts
@@ -6,25 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./championships.component.css']
 })
 export class ChampionshipsComponent  {
-  arrayOfChampions: Array<Object> = [
-    {year: 2015-2016,
+  arrayOfChampions: Array<any> = [
+    {year: "2015-2016",
     name:"Cleveland Cavaliers",
-    image: "https://camo.githubusercontent.com/8b4fb70df9be889e79171cbebf67127e233bb985/68747470733a2f2f73332d65752d776573742d312e616d617a6f6e6177732e636f6d2f69682d6d6174657269616c732f75706c6f6164732f75706c6f61645f34613130336135656261373030316437626537393065656335333533613434652e706e67",
-    players: "LBJ"},
+    image:"https://cbscleveland.files.wordpress.com/2014/12/cavslogo_770x433.jpg?w=770" ,
+    players: ["LBJ", "JR Smith", "Kevn Love", "Kyrie"]},
 
-    {year: 2014-2015,
-    name:"Golden State Warriors",
-    image: "https://camo.githubusercontent.com/8b4fb70df9be889e79171cbebf67127e233bb985/68747470733a2f2f73332d65752d776573742d312e616d617a6f6e6177732e636f6d2f69682d6d6174657269616c732f75706c6f6164732f75706c6f61645f34613130336135656261373030316437626537393065656335333533613434652e706e67",
-    players: "Curry"},
+    {year: "2007-2008",
+    name:"Boston Celtics",
+    image:"http://cdn8.bigcommerce.com/s-upa3n54fye/products/978/images/951/79411CP000__15792.1489697435.500.750.jpg?c=2",
+    players: ["Paul Pierce", "Rajon Rondo", "KG", "Ray Allen"]},
 
-    {year: 2013-2014,
+    {year: "2013-2014",
     name:"San Antonio Spurs",
-    image: "https://camo.githubusercontent.com/8b4fb70df9be889e79171cbebf67127e233bb985/68747470733a2f2f73332d65752d776573742d312e616d617a6f6e6177732e636f6d2f69682d6d6174657269616c732f75706c6f6164732f75706c6f61645f34613130336135656261373030316437626537393065656335333533613434652e706e67",
-    players: "Leonard"}
+    image: "https://sportslogohistory.com/wp-content/uploads/2017/12/san_antonio_spurs_2017-pres.png",
+    players: ["Leonard", "Duncan", "Manu", "Parker"]}
 
   ]
   constructor() { }
 
-  
 
 }
